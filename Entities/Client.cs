5f714a5cb412741a362ebb25df075eec1fe7878f
VitorCampos28 <vito.r.natal@hotmail.com>
2020-04-05 15:40:40 -0300

    Testando enum

--- Entities/Client.cs
+++ Entities/Client.cs
@@ -10,5 +10,16 @@ namespace Exercicio_de_Fixacao.Entities
         public string email { get; set; }
         public DateTime birthDay { get; set; }
 
+        public Client()
+        {
+
+        }
+
+        public Client(string name, string email, DateTime birthDay)
+        {
+            Name = name;
+            this.email = email;
+            this.birthDay = birthDay;
+        }
     }
 }
